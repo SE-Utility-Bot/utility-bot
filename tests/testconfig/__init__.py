d0250bb4a1a6340d3ec864742a12d845bfa23248
PlaceReporter99 <106759534+PlaceReporter99@users.noreply.github.com>
2023-07-31 11:05:16 +0100

    Update __init__.py

--- tests/testconfig/__init__.py
+++ tests/testconfig/__init__.py
@@ -9,14 +9,11 @@ def meta(a, b, c):
   """
   Metaclass for the Tests class, so that testing is as easy as doing Tests().
   """
-  return Ctuple(c.values())[2:]
-
-class Ctuple(tuple):
-  """
-  A class representing a callable tuple, which calls everything inside it. Used in the metaclass function.
-  """
-  def __call__(self, *args, **kwargs):
-    return Ctuple(x(*args, **kwargs) for x in self)
+  def func():
+    for x in list(c.values())[2:]:
+      x()
+  
+  return func
 
 class Tests(metaclass=meta):
   """
