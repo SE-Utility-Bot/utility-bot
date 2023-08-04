0dad76a64d6d28bab90da02b96a2c580d19ee4fb
Little Monkey <106759534+PlaceReporter99@users.noreply.github.com>
2023-08-04 16:14:23 +0100

    Update utilitybot.py
    Signed-off-by: Little Monkey <106759534+PlaceReporter99@users.noreply.github.com>

--- utilitybot.py
+++ utilitybot.py
@@ -101,33 +101,32 @@ def msg(event):
             )
         )
     elif event.content == "getcmd":
-        commands = [
-            "• echo <message>",
-            "• echochr <character number>",
-            "• calc <python expression>",
-            "• ping <user name>",
-            "• remotesay <room>, <message>",
-            "• getsource",
-            "• getcmd",
-            "• emptystring",
-            "• help",
-            "• op",
-            "• webscrape <URL>",
-        ]
+        commands = {
+            "echo <message>": "Sends the message given to it.",
+            "echochr <character number>": "Sends the unicode character with the codepoint of the number given to it. Must be in base 10.",
+            "calc <python expression>": "Sends the answer to the given Python expression. Uses a restricted character set due to security reasons.",
+            "ping <user name>": "Pings the person with the username that was passed to it.",
+            "remotesay <room>, <message>": "Sends a message in the specified room ID. If no room ID is given, the room defaults to Sandbox 2.",
+            "getsource": "Sends a link to the source code.",
+            "getcmd": "Lists the commands with their descriptions.",
+            "emptystring": "Sends a picture of an empty string.",
+            "help": "Shows some information.",
+            "op": "Replies with the message \"All systems operational!\". Exists to quickly check whether the bot is running.",
+            "webscrape <URL>": "Sends the HTML content of the specified URL.",
+        }
         r.send(
             r.buildReply(
                 event.message_id,
-                "Here are the available commands for this bot and their structures:",
+                "Here are the available commands for this bot and their structures:\n\n"+("\n".join(f'• {x}: {commands[x]}' for x in commands.keys())),
             )
         )
-        r.send("\n".join(commands))
     elif event.content == "emptystring":
         r.send(r.buildReply(event.message_id, "https://i.stack.imgur.com/Fh2Cq.png"))
     elif event.content == "help":
         r.send(
             r.buildReply(
                 event.message_id,
-                'Type in "getcmd" (without the quotes) for a list of commands.\n\nRepo: https://github.com/PlaceReporter99/utility-bot',
+                'Type in "getcmd" (without the quotes) for a list of commands and their descriptions.\n\nRepo: https://github.com/PlaceReporter99/utility-bot',
             )
         )
     elif event.content == "op":
