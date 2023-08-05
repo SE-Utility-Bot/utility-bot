0d3a6fa58b120a6d148df43675491b8f9e7b00c6
Little Monkey <106759534+PlaceReporter99@users.noreply.github.com>
2023-08-05 19:03:37 +0100

    Update sendmsg.py
    Signed-off-by: Little Monkey <106759534+PlaceReporter99@users.noreply.github.com>

--- sendmsg.py
+++ sendmsg.py
@@ -8,6 +8,8 @@ PASSWORD = sys.argv[2]
 EVENT_NAME = sys.argv[3]
 EVENT_USER = sys.argv[4]
 
+cleaned = re.sub(r'[.*]', '',EVENT_USER)
+
 bot = sechat.Bot()
 bot.login(EMAIL, PASSWORD)
 r = bot.joinRoom(147676)
@@ -18,5 +20,5 @@ def indent(text):
 
 
 r.send(
-    f'Event "{EVENT_NAME}" was triggered by {f"[{(cleaned := re.sub('[.*]','',EVENT_USER))}](https://github.com/{quote(cleaned)})"}.'
+    f'Event "{EVENT_NAME}" was triggered by {f"[{cleaned}](https://github.com/{cleaned})"}.'
 )
