7f458382039f03410ce925e2f1bdd280b47ec88c
Little Monkey <106759534+PlaceReporter99@users.noreply.github.com>
2023-08-05 19:09:32 +0100

    Update sendmsg.py (#11)
    Wow so many commits
    
    * Update sendmsg.py
    
    Signed-off-by: Little Monkey <106759534+PlaceReporter99@users.noreply.github.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Update sendmsg.py
    
    Signed-off-by: Little Monkey <106759534+PlaceReporter99@users.noreply.github.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Update sendmsg.py [no-test]
    
    Signed-off-by: Little Monkey <106759534+PlaceReporter99@users.noreply.github.com>
    
    * Update sendmsg.py
    
    Signed-off-by: Little Monkey <106759534+PlaceReporter99@users.noreply.github.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Update sendmsg.py
    
    Signed-off-by: Little Monkey <106759534+PlaceReporter99@users.noreply.github.com>
    
    * Update sendmsg.py
    
    Signed-off-by: Little Monkey <106759534+PlaceReporter99@users.noreply.github.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Update sendmsg.py
    
    Signed-off-by: Little Monkey <106759534+PlaceReporter99@users.noreply.github.com>
    
    * Update sendmsg.py
    
    Signed-off-by: Little Monkey <106759534+PlaceReporter99@users.noreply.github.com>
    
    * Update sendmsg.py
    
    Signed-off-by: Little Monkey <106759534+PlaceReporter99@users.noreply.github.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Update sendmsg.py
    
    Signed-off-by: Little Monkey <106759534+PlaceReporter99@users.noreply.github.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Signed-off-by: Little Monkey <106759534+PlaceReporter99@users.noreply.github.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- sendmsg.py
+++ sendmsg.py
@@ -1,13 +1,24 @@
 import sechat
 import sys
+import re
+from urllib.parse import quote
 
 EMAIL = sys.argv[1]
 PASSWORD = sys.argv[2]
 EVENT_NAME = sys.argv[3]
-EVENT_DATA = sys.argv[4]
+EVENT_USER = sys.argv[4]
+
+cleaned = re.sub(r"\[.*\]", "", EVENT_USER)
 
 bot = sechat.Bot()
 bot.login(EMAIL, PASSWORD)
 r = bot.joinRoom(147676)
 
-r.send(f'Event "{EVENT_NAME}" occured. Data: {EVENT_DATA}')
+
+def indent(text):
+    return "\n".join("    " + x for x in text.split("\n"))
+
+
+r.send(
+    f'Event "{EVENT_NAME}" was triggered by {f"[{cleaned}](https://github.com/{cleaned})"}.'
+)
