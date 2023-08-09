a441beb4cba32e54627fb84a600a93acb9d1b2fe
Little Monkey <106759534+PlaceReporter99@users.noreply.github.com>
2023-08-09 18:58:25 +0100

    Update utilitybot.py (#15)
    * Update utilitybot.py
    
    Signed-off-by: Little Monkey <106759534+PlaceReporter99@users.noreply.github.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Update utilitybot.py
    
    Signed-off-by: Little Monkey <106759534+PlaceReporter99@users.noreply.github.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Signed-off-by: Little Monkey <106759534+PlaceReporter99@users.noreply.github.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- utilitybot.py
+++ utilitybot.py
@@ -113,26 +113,31 @@ def roomer(r):
                 "ping <user name>": "               Pings the person with the username that was passed to it.",
                 "remotesay <room>, <message>": "    Sends a message in the specified room ID. If no room ID is given, the room defaults to Sandbox 2.",
                 "getsource": "                      Sends a link to the source code.",
-                "getcmd": "                         Lists the commands with their descriptions.",
+                "getcmd <command>": "               Sends the command description. If no command is given, it lists the commands with their descriptions instead.",
                 "emptystring": "                    Sends a picture of an empty string.",
                 "help": "                           Shows some information.",
                 "op": '                             Replies with the message "All systems operational.". Exists to quickly check whether the bot is running.',
                 "webscrape <URL>": "                Sends the HTML content of the specified URL.",
             }
             if len(event.content) > 6:
-                r.send(
-                    r.buildReply(
-                        event.message_id,
-                        "`"+(
-                            result := [
-                                x
-                                for x in commands.keys()
-                                if re.match(event.content.partition(" ")[2], x)
-                            ][0]
-                        )+"`: "
-                        + commands[result],
+                try:
+                    r.send(
+                        r.buildReply(
+                            event.message_id,
+                            "`"
+                            + (
+                                result := [
+                                    x
+                                    for x in commands.keys()
+                                    if re.match(event.content.partition(" ")[2], x)
+                                ][0]
+                            )
+                            + "`: "
+                            + commands[result],
+                        )
                     )
-                )
+                except IndexError:
+                    r.send(r.buildReply(event.message_id, "Command does not exist."))
             else:
                 r.send(
                     indent(
