db4a74b1d7513d5246e41b46b919ca4b7be51dbf
Little Monkey <106759534+PlaceReporter99@users.noreply.github.com>
2023-08-10 09:45:18 +0100

    Create manual_test.py
    Signed-off-by: Little Monkey <106759534+PlaceReporter99@users.noreply.github.com>

---
+++ manual_test.py
@@ -0,0 +1,9 @@
+from utilitybot import roomer
+import sechat
+from sechat.events import Events
+
+bot = sechat.Bot()
+bot.login(sys.argv[1], sys.argv[2])
+r = bot.joinRoom(147805)
+r.send("PR test deployed. You have 6 hours to test it.")
+r.on(Events.MESSAGE, roomer(r))
