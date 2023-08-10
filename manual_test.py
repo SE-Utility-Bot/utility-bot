aa20d70f97ffcf66fdadbcf2c87c3c3145e621c1
Little Monkey <106759534+PlaceReporter99@users.noreply.github.com>
2023-08-10 10:20:00 +0100

    Update manual_test.py
    Signed-off-by: Little Monkey <106759534+PlaceReporter99@users.noreply.github.com>

--- manual_test.py
+++ manual_test.py
@@ -6,4 +6,11 @@ bot = sechat.Bot()
 bot.login(sys.argv[1], sys.argv[2])
 r = bot.joinRoom(147805)
 r.send("PR test deployed. You have 6 hours to test it.")
+
 r.on(Events.MESSAGE, roomer(r))
+
+try:
+  while True:
+    pass
+finally:
+  bot.leaveAllRooms()
