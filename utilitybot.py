a2996968e607728b03ea756545ccfb69059545d8
Little Monkey <106759534+PlaceReporter99@users.noreply.github.com>
2023-08-19 14:41:54 +0100

    Update utilitybot.py
    Signed-off-by: Little Monkey <106759534+PlaceReporter99@users.noreply.github.com>

--- utilitybot.py
+++ utilitybot.py
@@ -120,17 +120,18 @@ def roomer(r):
             )
         elif event.content[:6] == "getcmd":
             commands = {
-                "echo <message>": "                 Sends the message given to it.",
-                "echochr <character number>": "     Sends the unicode character with the codepoint of the number given to it. Must be in base 10.",
-                "calc <python expression>": "       Sends the answer to the given Python expression. Uses a restricted character set due to security reasons.",
-                "ping <user name>": "               Pings the person with the username that was passed to it.",
-                "remotesay <room>, <message>": "    Sends a message in the specified room ID. If no room ID is given, the room defaults to Sandbox 2.",
-                "getsource": "                      Sends a link to the source code.",
-                "getcmd <command>": "               Sends the command description. If no command is given, it lists the commands with their descriptions instead.",
-                "emptystring": "                    Sends a picture of an empty string.",
-                "help": "                           Shows some information.",
-                "op": '                             Replies with the message "All systems operational.". Exists to quickly check whether the bot is running.',
-                "webscrape <URL>": "                Sends the HTML content of the specified URL.",
+                "echo <message>": "                      Sends the message given to it.",
+                "echochr <character number>": "          Sends the unicode character with the codepoint of the number given to it. Must be in base 10.",
+                "calc <python expression>": "            Sends the answer to the given Python expression. Uses a restricted character set due to security reasons.",
+                "ping <user name>": "                    Pings the person with the username that was passed to it.",
+                "remotesay <room>, <message>": "         Sends a message in the specified room ID. If no room ID is given, the room defaults to Sandbox 2.",
+                "getsource": "                           Sends a link to the source code.",
+                "getcmd <command>": "                    Sends the command description. If no command is given, it lists the commands with their descriptions instead.",
+                "emptystring": "                         Sends a picture of an empty string.",
+                "help": "                                Shows some information.",
+                "op": '                                  Replies with the message "All systems operational.". Exists to quickly check whether the bot is running.',
+                "webscrape <URL>": "                     Sends the HTML content of the specified URL.",
+                "random <quantity>, <start>, <end>": "   Sends the specified number of random numbers in the inclusive range (using os.urandom). 1 argument uses the range 0 to 255, and 2 arguments uses the range 0 to <end>.",
             }
             if len(event.content) > 6:
                 try:
@@ -186,6 +187,14 @@ def roomer(r):
                     ),
                 )
             )
+        elif event.content[:7] == "random ":
+            args = [int(x) for x in event.content[7:].split(',')]
+            if len(args) == 1:
+                r.send(r.buildReply(event.message_id, "Here are your random numbers: " + str([*os.urandom(args[0])])))
+            elif len(args) == 2:
+                r.send(r.buildReply(event.message_id, "Here are your random numbers: " + str([(args[1] * x) // 255 for x in os.urandom(args[0])])))
+            elif len(args) == 3:
+                r.send(r.buildReply(event.message_id, "Here are your random numbers: " + str([(((args[2] - args[1]) * x) // 255) + args[1] for x in os.urandom(args[0])])))
 
     return msg
 
