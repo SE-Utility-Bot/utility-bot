4dfc5b7602fbb1d287ff9fc3cffc73d8a4d4e0ca
PlaceReporter99 <106759534+PlaceReporter99@users.noreply.github.com>
2023-08-26 18:37:03 +0100

    Update utilitybot.py

--- utilitybot.py
+++ utilitybot.py
@@ -29,7 +29,7 @@ def indent(text):
 def remove_lead_space(text):
     it = iter(text)
     while (result := next(it)) == " ":
-        pass
+        """This chips off leading spaces from the iterator."""
     return result + "".join(it)
 
 
