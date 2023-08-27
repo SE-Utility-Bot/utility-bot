13c4a0731aee2cde072a3902e130e9eb345cf18a
PlaceReporter99 <106759534+PlaceReporter99@users.noreply.github.com>
2023-08-27 17:46:57 +0100

    stuff

--- utilitybot.py
+++ utilitybot.py
@@ -4,6 +4,7 @@ import re
 import subprocess
 import sys
 import time
+import secrets
 from urllib.request import urlopen
 
 import sechat
@@ -158,11 +159,11 @@ def roomer(r):
                 "help":
                 "                                Shows some information.",
                 "op":
-                '                                  Replies with the message "All systems operational.". Exists to quickly check whether the bot is running.',
+                '                                  Replies with a random message from statuses.txt. Exists to quickly check whether the bot is running.',
                 "webscrape <URL>":
                 "                     Sends the HTML content of the specified URL.",
                 "random <quantity>, <start>, <end>":
-                "   Sends the specified number of random numbers in the inclusive range (using os.urandom). 1 argument uses the range 0 to 255, and 2 arguments uses the range 0 to <end>. Maximum argument value is 1000 for <quantity> and 10 ** 24 for all other arguments. The numbers lose a little bit of accuracy as the number of possible numbers go up.",
+                "   Sends the specified number of random numbers in the inclusive range (using secrets.choice). 1 argument uses the range 0 to 255, and 2 arguments uses the range 0 to <end>. Maximum argument value is 1000 for <quantity> and 9 * 10 ** 18 for all other arguments.",
                 "translate <text> | <to> | <from>":
                 "      Translates <text> from the language code in <from> (automatically detects language if none is given) to the language code in <to> (translates to English if none is given). See https://placereporter99.github.io/utility-bot/supported-langs/ for supported languages and their language codes.",
             }
@@ -197,7 +198,8 @@ def roomer(r):
                     'Type in "getcmd" (without the quotes) for a list of commands and their descriptions.\n\nRepo: https://github.com/PlaceReporter99/utility-bot',
                 ))
         elif event.content == "op":
-            r.send(r.buildReply(event.message_id, "All systems operational."))
+            with open("status.txt") as f:
+                r.send(r.buildReply(event.message_id, secrets.choice(f.read().split('\n'))))
         elif event.content[:10] == "webscrape ":
             r.send(
                 r.buildReply(
@@ -208,37 +210,19 @@ def roomer(r):
                 ))
         elif event.content[:7] == "random ":
             args = [int(x) for x in event.content[7:].split(",")]
-            if args[0] > 1000 or any(x > 1000000000000000000000000
-                                     for x in args):
+            if len(args) == 1:
+                args.append(0)
+                args.append(255)
+            elif len(args) == 2:
+                args.insert(1, 0)
+            if args[0] > 1000 or any(x > 9 * 10 ** 18 for x in args):
                 r.send(
                     r.buildReply(
                         event.message_id,
                         "Sorry, that will probably take me too long."))
-            elif len(args) == 1:
-                r.send(
-                    r.buildReply(
-                        event.message_id,
-                        "Here are your random numbers:\n" +
-                        str([*os.urandom(args[0])]),
-                    ))
-            elif len(args) == 2:
-                r.send(
-                    r.buildReply(
-                        event.message_id,
-                        "Here are your random numbers:\n" + str([
-                            a if (a := ((args[1] + 1) * x) //
-                                  255) <= args[1] else args[1]
-                            for x in os.urandom(args[0])
-                        ]),
-                    ))
-            elif len(args) == 3:
-                r.send(
-                    r.buildReply(
-                        event.message_id,
-                        "Here are your random numbers:\n" +
-                        str([(((args[2] - args[1] + 1) * x) // 255) + args[1]
-                             for x in os.urandom(args[0])]),
-                    ))
+            else:
+                numbers = [secrets.choice(range(args[1], args[2] + 1)) for x in range(args[0])]
+                r.send(r.buildReply(event.message_id, f"Here are your random numbers:\n{numbers}"))
         elif event.content[:10] == "translate ":
             arguments = [
                 remove_space(x) for x in event.content[10:].split("|")
