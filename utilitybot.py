14c623a595704cb821cf2380a596c000edc1ddac
PlaceReporter99 <106759534+PlaceReporter99@users.noreply.github.com>
2023-08-28 09:29:00 +0100

    Fix regex match

--- utilitybot.py
+++ utilitybot.py
@@ -48,7 +48,7 @@ def remote(event):
 def roomer(r):
 
     def msg(event):
-        if (result := re.match(html.unescape(event.content), r"🐟 <i>(.*)'s line quivers\.<\/i>", re.UNICODE)) and event.user_id == 358560:
+        if (result := re.match(r"🐟 <i>(.*)'s line quivers\.<\/i>", html.unescape(event.content), re.UNICODE)) and event.user_id == 358560:
             r.send(f"@{result.group(1).replace(' ', '')} your fish is ready!")
         elif event.content[:5] == "echo ":
             r.send(html.unescape(event.content[5:]))
