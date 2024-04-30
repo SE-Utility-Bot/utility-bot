d66b010d82c6d6da706e031667b618d8f9df1895
PlaceReporter99 <106759534+PlaceReporter99@users.noreply.github.com>
2024-04-30 18:58:23 +0100

    use main.py and some other file name changes and code movement

--- sendmsg.py
+++ sendmsg.py
@@ -1,28 +1,13 @@
-import re
-import sys
 import os
 
-import requests
 import sechat
 
 EMAIL = os.environ["email"]
 PASSWORD = os.environ["password"]
-EVENT_NAME = os.environ["event"]
-EVENT_USER = os.environ["actor"]
-PAYLOAD = os.environ["payload"]
-PASTEBIN_KEY = os.environ["pastebin"]
-
-CLEANED = re.sub(r"\[.*\]", "", EVENT_USER)
+MSG = os.environ["message"]
+ROOM = os.environ["room"]
 
 bot = sechat.Bot()
 bot.login(EMAIL, PASSWORD)
-r = bot.joinRoom(152883)
-
-def pastebin(text, key):
-    return requests.post("https://pastebin.com/api/api_post.php", data = {'api_dev_key': key, 'api_option': 'paste', 'api_paste_code': text, 'api_paste_private': '1'}).text
-
-def indent(text):
-    return "\n".join("    " + x for x in text.split("\n"))
-
-
-r.send(f'Event "{EVENT_NAME}" was triggered by [{CLEANED}](https://github.com/{CLEANED}). The [payload]({pastebin(PAYLOAD, PASTEBIN_KEY)}) is linked.')
+r = bot.joinRoom(int(ROOM))
+r.send(MSG)
