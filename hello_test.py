5896b629ecd46e734b6eb9a49a2ad4184b382203
Little Monkey <164184648+PlaceReporter98@users.noreply.github.com>
2026-07-01 18:36:11 +0100

    Create hello_test.py
    Signed-off-by: Little Monkey <164184648+PlaceReporter98@users.noreply.github.com>

---
+++ hello_test.py
@@ -0,0 +1,10 @@
+import asyncio
+
+from sechat import Credentials, Room
+
+async def main():
+    credentials = await Credentials.load_or_authenticate("credentials.dat", os.environ["BOT_EMAIL"], os.environ["BOT_PASSWORD"])
+    async with Room.join(credentials, 1) as room:
+        await room.send("Hello World!")
+
+asyncio.run(main())
